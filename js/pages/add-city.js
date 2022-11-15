b8eb4ef94e8702aac017f061fd7e39991206a7ca
valentinogambino <gambinovt@gmail.com>
2022-11-15 18:48:42 -0300

    primer commit

---
+++ js/pages/add-city.js
@@ -0,0 +1,15 @@
+function getCitiesFromLocalStorage() {
+    let cities = localStorage.getItem("CITIES");
+    if(cities) {
+        cities = JSON.parse(cities);
+    } else {
+        cities = [];
+    }
+    return cities;
+}
+
+function addNewCityToLocalStorage(newCity) {
+    let cities = getCitiesFromLocalStorage();
+    cities.push(newCity);
+    localStorage.setItem("CITIES", JSON.stringify(cities));
+}
